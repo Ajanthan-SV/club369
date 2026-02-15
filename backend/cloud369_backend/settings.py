54740bf7af05445eb022697fd10ef9923eed764a
Ajanthan-SV <harissanthisurya@gmail.com>
2026-02-15 14:29:18 +0530

    token timing changes

--- backend/cloud369_backend/settings.py
+++ backend/cloud369_backend/settings.py
@@ -156,8 +156,8 @@ REST_FRAMEWORK = {
 
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
